d08e19d61382633c15c979efe8fb339fb2341972
Anand Shekhar <66034323+anandshekhr@users.noreply.github.com>
2025-12-02 12:11:02 +0530

    Fix routes

--- src/app/features/products/products-routing.module.ts
+++ src/app/features/products/products-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products.component';
+import { RealtimeMonitoringComponent } from './pages/realtime-monitoring/realtime-monitoring.component';
+import { EnergyMonitoringComponent } from './pages/energy-monitoring/energy-monitoring.component';
+import { RoboticsIntegrationComponent } from './pages/robotics-integration/robotics-integration.component';
+import { ProductionSchedulingComponent } from './pages/production-scheduling/production-scheduling.component';
+import { PredictiveMaintenanceComponent } from './pages/predictive-maintenance/predictive-maintenance.component';
+import { MaterialRfidTrackingComponent } from './pages/material-rfid-tracking/material-rfid-tracking.component';
 
-const routes: Routes = [{ path: '', component: ProductsComponent }];
+const routes: Routes = [
+  { path: '', component: ProductsComponent },
+  { path: 'realtime-monitoring', component: RealtimeMonitoringComponent },
+  { path: 'energy-monitoring', component: EnergyMonitoringComponent },
+  { path: 'robotics-integration', component: RoboticsIntegrationComponent },
+  { path: 'production-scheduling', component: ProductionSchedulingComponent },
+  { path: 'predictive-maintenance', component: PredictiveMaintenanceComponent },
+  { path: 'material-rfid-tracking', component: MaterialRfidTrackingComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
